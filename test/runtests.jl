bb0fd0f1bd35cb920255d9b380b9f19ad7c3bb63
Beforerr <zzj956959688@gmail.com>
2024-11-17 00:58:51 -0800

    test: Refactor the runtests.jl file into mulitple files

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,28 +7,7 @@ using Test
     end
 
     @testset "permute_args function" begin
-        # Define base function
-        function base_func(x::Int, y::String)
-            return "x=$x, y=$y"
-        end
-
-        # Create permutable version
-        permuted_func = permute_args(base_func, [Int, String])
-
-        # Test normal order
-        @test permuted_func(42, "hello") == base_func(42, "hello")
-
-        # Test reversed order
-        @test permuted_func("hello", 42) == base_func(42, "hello")
-
-        # Test type errors
-        @test_throws MethodError permuted_func(1.0, "hello")
-        @test_throws MethodError permuted_func("hello", "world")
-
-        # Test wrong number of arguments
-        @test_throws ArgumentError permuted_func(42)
-        @test_throws ArgumentError permuted_func(42, "hello", 3.14)
-
+        include("function.jl")
     end
 
     @testset "permute_args! function" begin
