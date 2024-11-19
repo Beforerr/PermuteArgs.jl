65120b5f441c8b15910ce0ce621d4e653d335aa0
Beforerr <zzj956959688@gmail.com>
2024-11-18 20:03:39 -0800

    chore: add benchmarks

---
+++ benchmarks/runbencmarks.jl
@@ -0,0 +1,22 @@
+using Chairmarks
+using PermuteArgs
+
+function base_func(x::Int, y::String, z::Float64)
+    return x + length(y) + z
+end
+
+@permute_args function permuted_base_func(x::Int, y::String, z::Float64)
+    return x + length(y) + z
+end
+
+permuted_func = permute_args(base_func)
+
+@b base_func(42, "hello", 3.14)
+
+@b permuted_func(42, "hello", 3.14)
+
+@b permuted_func(3.14, "hello", 42)
+
+@b permuted_base_func("hello", 42, 3.14)
+
+@b permuted_base_func(42, "hello", 3.14)
