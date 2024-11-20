ed276438b6ea549fd6f2cf1daf1153e021d0f7be
Beforerr <zzj956959688@gmail.com>
2024-11-19 23:06:26 -0800

    chore: deprecate permute_args_dynamic

--- src/function.jl
+++ src/function.jl
@@ -28,42 +28,6 @@ function permute_args(m::Method; mod=nothing)
 end
 
 """
-    permute_args_dynamic(m::Method)
-
-Create a new function that allows arbitrary argument order based on the method signature.
-
-This returned function is not type-stable and internally uses a lookup table to reorder arguments.
-"""
-function permute_args_dynamic(m::Method)
-    types = m.sig.types[2:end]
-    func = m.sig.types[1].instance
-    n = length(types)
-
-    # Precompute all permutations and their inverse mappings
-    perms = permutations(1:n)
-    # Create a lookup dictionary for each permutation
-    perm_lookups = Tuple((types[p], inverse_permutation(p)) for p in perms)
-
-    # Create new function with type-stable internals
-    new_fund_ex = quote
-        function (args::Vararg{Any,$n})
-            arg_types = ntuple(i -> typeof(args[i]), $n)
-            # Find matching permutation using precomputed lookups
-            for (expected_type, inverse_perm) in $perm_lookups
-                if all(i -> arg_types[i] <: expected_type[i], 1:$n)
-                    # Use precomputed lookup for reordering
-                    ordered_args = ntuple(i -> args[inverse_perm[i]], $n)
-                    return $func(ordered_args...)
-                end
-            end
-            $func(args...)
-        end
-    end
-
-    return eval(new_fund_ex)
-end
-
-"""
     permute_args(f[, types])
 
 Returns a new function for function `f` allowing arbitrary argument order based on the provided types.
