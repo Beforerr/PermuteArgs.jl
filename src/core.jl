75decbc718215d9ab83eabb0161a9949ad43136a
Beforerr <zzj956959688@gmail.com>
2025-01-30 23:04:45 -0800

    refactor: reuse generation function

--- src/core.jl
+++ src/core.jl
@@ -1,6 +1,15 @@
 codegen_ast_func(; kw...) = codegen_ast(JLFunction(; kw...))
 codegen_ast_call(; kw...) = codegen_ast(JLCall(; kw...))
 
+"""
+Generate a function definition with arguments in any order.
+
+The following arguments are directly passed to the [`JLFunction`](@ref) constructor:
+- `name`: The name of the function.
+- `args`: A vector of argument names.
+- `body`: The body of the function.
+- `kwargs`: A vector of keyword argument names.
+"""
 function generate_permuted_methods(args; perms=nothing, kw...)
     perms = something(perms, permutations(1:length(args)))
     return map(perms) do p
@@ -23,13 +32,11 @@ function generate_permutable_struct(expr)
     ismutable, name, typevars, supertype, body = split_struct(expr)
     fields = split_struct_body(body)
 
-    body = Expr(:call, name, fields...)
+    body = codegen_ast_call(; func=name, args=fields)
 
     # Generate outer constructors for all permutations
     perms = collect(permutations(1:length(fields)))[2:end]
-    constructors = map(perms) do p
-        codegen_ast_func(; name, args=fields[p], body)
-    end
+    constructors = generate_permuted_methods(fields; name, body, perms)
 
     # Generate keyword constructor
     kw_constructor = codegen_ast_func(; name, kwargs=fields, body)
