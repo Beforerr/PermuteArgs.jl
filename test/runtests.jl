860c0702b284720b020776b6f576bed640d33afe
Zijin Zhang <58776897+Beforerr@users.noreply.github.com>
2026-05-16 00:14:55 -0700

    refactor: simplify test structure (#12)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,16 +1,14 @@
 using PermuteArgs
 using Test
 
-@testset "PermuteArgs.jl" begin
-    @testset "@permute_args macro" begin
-        include("macro.jl")
-    end
+@testset "@permute_args macro" begin
+    include("macro.jl")
+end
 
-    @testset "permute_args function" begin
-        include("function.jl")
-    end
+@testset "permute_args function" begin
+    include("function.jl")
+end
 
-    @testset "permute_args! function" begin
-        include("inplace_permute.jl")
-    end
+@testset "permute_args! function" begin
+    include("inplace_permute.jl")
 end
